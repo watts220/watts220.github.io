b6bb66a8e9eaa6eb6826ee4b519e999c02306240
Александр Цыбуля <watts220@gmail.com>
2018-03-28 15:57:35 +0300

    fix sw file

--- sw.js
+++ sw.js
@@ -1,19 +1,30 @@
 self.addEventListener('install', e => {
- e.waitUntil(
-   // после установки service worker
-   // открыть новый кэш
-   caches.open('my-pwa-cache').then(cache => {
-     // добавляем все URL ресурсов, которые хотим закэшировать
-     return cache.addAll([
-       '/',
-       '/index.html',
-       '/about.html',
-       '/contact.html',
-       '/portfolio.html',
-       '/services.html',
-       '/css/main.css',
-       '/js/main.js',
-     ]);
-   })
- );
+  const timeStamp = Date.now();
+  e.waitUntil(
+    caches.open('geekqlick-cache').then(cache => {
+      return cache.addAll([
+        `/`,
+        `/index.html?timestamp=${timeStamp}`,
+        `/about.html?timestamp=${timeStamp}`,
+        `/contact.html?timestamp=${timeStamp}`,
+        `/portfolio.html?timestamp=${timeStamp}`,
+        `/services.html?timestamp=${timeStamp}`,
+        `/css/main.css?timestamp=${timeStamp}`,
+        `/js/main.js?timestamp=${timeStamp}`
+      ])
+          .then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch: true}).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
